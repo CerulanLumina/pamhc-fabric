d040257db5e51949b884c219e0c8c606f89407b8
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-09-06 16:41:28 -0400

    Make the presser block entity craft!

--- src/main/java/net/cerulan/harvestcraftfabric/blockentity/PresserBlockEntity.java
+++ src/main/java/net/cerulan/harvestcraftfabric/blockentity/PresserBlockEntity.java
@@ -3,6 +3,8 @@ package net.cerulan.harvestcraftfabric.blockentity;
 import io.github.cottonmc.cotton.gui.PropertyDelegateHolder;
 import net.cerulan.harvestcraftfabric.gui.PresserGuiDescription;
 import net.cerulan.harvestcraftfabric.inventory.MachineInventory;
+import net.cerulan.harvestcraftfabric.recipe.DoubleOutputRecipe;
+import net.cerulan.harvestcraftfabric.recipe.RecipeRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.InventoryProvider;
 import net.minecraft.block.entity.BlockEntity;
@@ -10,7 +12,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.recipe.Recipe;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
@@ -20,6 +24,8 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldAccess;
+
+import java.util.Optional;
 
 public class PresserBlockEntity extends BlockEntity implements Tickable,
         InventoryProvider,
@@ -35,9 +41,12 @@ public class PresserBlockEntity extends BlockEntity implements Tickable,
         @Override
         public int get(int index) {
             switch (index) {
-                case 0: return ticksProgress;
-                case 1: return MAXIMUM_PROGRESS;
-                default: throw new UnsupportedOperationException();
+                case 0:
+                    return ticksProgress;
+                case 1:
+                    return MAXIMUM_PROGRESS;
+                default:
+                    throw new UnsupportedOperationException();
             }
         }
 
@@ -63,8 +72,30 @@ public class PresserBlockEntity extends BlockEntity implements Tickable,
 
     @Override
     public void tick() {
-        if (ticksProgress == MAXIMUM_PROGRESS) ticksProgress = 0;
-        else ++ticksProgress;
+        assert world != null;
+        if (world.isClient) return;
+        Optional<DoubleOutputRecipe> recipe = world.getRecipeManager().getFirstMatch(RecipeRegistry.PRESSER, this.inventory, world);
+        if (recipe.isPresent()) {
+            ItemStack output = recipe.get().getOutput();
+            ItemStack output2 = recipe.get().getOutput2();
+            if (canOutput(output, 1) && canOutput(output2, 2)) {
+                if (ticksProgress == MAXIMUM_PROGRESS) {
+                    ticksProgress = 0;
+                    if (inventory.getStack(1).isEmpty()) inventory.setStack(1, output);
+                    else inventory.getStack(1).increment(output.getCount());
+                    if (inventory.getStack(2).isEmpty()) inventory.setStack(2, output2);
+                    else
+                        inventory.getStack(2).increment(output2.getCount());
+                    inventory.getStack(0).decrement(1);
+                } else ++ticksProgress;
+            } else ticksProgress = 0;
+        }
+        else ticksProgress = 0;
+    }
+
+    private boolean canOutput(ItemStack stack, int slot) {
+        ItemStack existing = inventory.getStack(slot);
+        return existing.isEmpty() || (existing.isItemEqual(stack) && ItemStack.areTagsEqual(existing, stack) && existing.getCount() + stack.getCount() <= stack.getMaxCount());
     }
 
     @Override
