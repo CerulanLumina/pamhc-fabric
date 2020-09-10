23f53251c1fe025e3373f302a43af55747e16118
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-09-10 17:00:40 -0400

    Fix using wrong tag for Shipping Bin, slightly adjust shipping bin ui

--- src/main/java/net/cerulan/harvestcraftfabric/gui/ShippingBinGuiDescription.java
+++ src/main/java/net/cerulan/harvestcraftfabric/gui/ShippingBinGuiDescription.java
@@ -12,26 +12,26 @@ import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class ShippingBinGuiDescription extends SyncedGuiDescription {
-    public static final Identifier buyable = LocalPam.modID("buyable");
+    public static final Identifier sellable = LocalPam.modID("sellable");
     private static final int INVENTORY_SIZE = 10;
-    private final Tag<Item> buyableTag;
+    private final Tag<Item> sellableTag;
     public ShippingBinGuiDescription(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(GuiRegistry.SHIPPINGBIN_HANDLER_TYPE, syncId, playerInventory, getBlockInventory(context, INVENTORY_SIZE), getBlockPropertyDelegate(context));
-        buyableTag = TagRegistry.item(buyable);
+        sellableTag = TagRegistry.item(sellable);
         WGridPanel root = new WGridPanel();
 
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
                 WItemSlot inputSlot = WItemSlot.of(blockInventory, 1 + i * 3 + j);
-                inputSlot.setFilter(stack -> buyableTag.contains(stack.getItem()));
-                root.add(inputSlot, i, j);
+                inputSlot.setFilter(stack -> sellableTag.contains(stack.getItem()));
+                root.add(inputSlot, 2 + j, 1 + i);
             }
         }
 
         WItemSlot outputSlot = WItemSlot.outputOf(blockInventory, 0).setInsertingAllowed(false);
-        root.add(outputSlot, 4, 1);
+        root.add(outputSlot, 6, 2);
 
-        root.add(createPlayerInventoryPanel(), 0, 3);
+        root.add(createPlayerInventoryPanel(), 0, 4);
 
         root.validate(this);
         setRootPanel(root);
